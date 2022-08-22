9590077a0bfa6bd95f366d8ffe254b354a6e0287
Beth <bethmelmtv@gmail.com>
2022-08-21 23:02:36 -0700

    exploring tailwind to add to affirmations feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { getAffirmation } from './services/fetch-utils';
-// import { getAffirmation } from './services/fetch-utils';
+import { useState } from 'react';
 
 function App() {
+  const [affirmation, setAffirmation] = useState('');
+
   async function handleSubmit() {
     const response = await getAffirmation();
     console.log('response', response);
+    setAffirmation(response);
     return response;
   }
   return (
     <div className="App">
       <button onClick={handleSubmit}>Get Affirmation</button>
+      <p>{affirmation.affirmation}</p>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff4800',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
